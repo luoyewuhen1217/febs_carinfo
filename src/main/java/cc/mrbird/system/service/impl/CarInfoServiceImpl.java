148e98e2ad282e30bc60486ca4106f90e78d22b8
qiweb2018 <zhangqi@dbgo.cn>
2018-10-28 17:44:51 +0800

    张奇2018 1028 修改 排序条件 istop desc倒序i

--- src/main/java/cc/mrbird/system/service/impl/CarInfoServiceImpl.java
+++ src/main/java/cc/mrbird/system/service/impl/CarInfoServiceImpl.java
@@ -57,8 +57,8 @@ public class CarInfoServiceImpl extends BaseService<CarInfo> implements CarInfoS
 //            if (StringUtils.isNotBlank(carinfo.getEmissionStandard())) {
 //                criteria.andCondition("emissionStandard=", Long.valueOf(carinfo.getEmissionStandard()));
 //            }
-            example.setOrderByClause("ISTOP");
-            example.setOrderByClause("CAR_ID");
+            example.setOrderByClause("ISTOP desc");
+            //example.setOrderByClause("CAR_ID");
             System.out.println("example3:::"+example.getCountColumn());
             return this.selectByExample(example);
         } catch (NumberFormatException e) {
