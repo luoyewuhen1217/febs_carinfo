c53af63957355020485b11b7cfe39e04595fe171
luoyewuhen1217 <455684145@qq.com>
2018-12-08 09:42:33 +0800

    添加会员订单

---
+++ src/main/resources/static/js/app/system/order/user.js
@@ -0,0 +1,140 @@
+$(function () {
+    var $userTableForm = $(".user-table-form");
+    var settings = {
+        url: ctx + "user/list",
+        pageSize: 10,
+        queryParams: function (params) {
+            return {
+                pageSize: params.limit,
+                pageNum: params.offset / params.limit + 1,
+                username: $userTableForm.find("input[name='username']").val().trim(),
+                timeField: $userTableForm.find("input[name='timeField']").val().trim(),
+                ssex: $userTableForm.find("select[name='ssex']").val(),
+                status: $userTableForm.find("select[name='status']").val()
+            };
+        },
+        columns: [{
+            checkbox: true
+        }, {
+            field: 'userId',
+            visible: false
+        }, {
+            field: 'username',
+            title: '用户名'
+        // }, {
+        //     field: 'deptName',
+        //     title: '部门'
+        // }, {
+        //     field: 'email',
+        //     title: '邮箱',
+        //     width:120,
+        //     class:'colStyle',
+        //     formatter:paramsMatter
+        // }, {
+        //     field: 'mobile',
+        //     title: '手机'
+        // }, {
+        //     field: 'ssex',
+        //     title: '性别',
+        //     formatter: function (value, row, index) {
+        //         if (value === '0') return '男';
+        //         else if (value === '1') return '女';
+        //         else return '保密';
+        //     }
+        }, {
+            field: 'crateTime',
+            title: '注册时间'
+        }, {
+            field: 'status',
+            title: '状态',
+            formatter: function (value, row, index) {
+                if (value === '1') return '<span class="badge badge-success">有效</span>';
+                if (value === '0') return '<span class="badge badge-warning">锁定</span>';
+            }
+        }
+
+        ]
+    };
+
+    $MB.initTable('userTable', settings);
+    $MB.calenders('input[name="timeField"]', true, false);
+});
+
+function search() {
+    $MB.refreshTable('userTable');
+}
+
+function refresh() {
+    $(".user-table-form")[0].reset();
+    $MB.refreshTable('userTable');
+}
+
+function deleteUsers() {
+    var selected = $("#userTable").bootstrapTable('getSelections');
+    var selected_length = selected.length;
+    var contain = false;
+    if (!selected_length) {
+        $MB.n_warning('请勾选需要删除的用户！');
+        return;
+    }
+    var ids = "";
+    for (var i = 0; i < selected_length; i++) {
+        ids += selected[i].userId;
+        if (i !== (selected_length - 1)) ids += ",";
+        if (userName === selected[i].username) contain = true;
+    }
+    if (contain) {
+        $MB.n_warning('勾选用户中包含当前登录用户，无法删除！');
+        return;
+    }
+
+    $MB.confirm({
+        text: "确定删除选中用户？",
+        confirmButtonText: "确定删除"
+    }, function () {
+        $.post(ctx + 'user/delete', {"ids": ids}, function (r) {
+            if (r.code === 0) {
+                $MB.n_success(r.msg);
+                refresh();
+            } else {
+                $MB.n_danger(r.msg);
+            }
+        });
+    });
+}
+
+function exportUserExcel() {
+    $.post(ctx + "user/excel", $(".user-table-form").serialize(), function (r) {
+        if (r.code === 0) {
+            window.location.href = "common/download?fileName=" + r.msg + "&delete=" + true;
+        } else {
+            $MB.n_warning(r.msg);
+        }
+    });
+}
+
+function exportUserCsv() {
+    $.post(ctx + "user/csv", $(".user-table-form").serialize(), function (r) {
+        if (r.code === 0) {
+            window.location.href = "common/download?fileName=" + r.msg + "&delete=" + true;
+        } else {
+            $MB.n_warning(r.msg);
+        }
+    });
+}
+
+function paramsMatter(value, row, index) {
+    var values = row.email;
+    var span=document.createElement('span');
+    span.setAttribute('title',values);
+    span.innerHTML = row.email;
+    return span.outerHTML;
+}
+
+// function paramsMatter2(value, row, index) {
+//     var values = row.email;//获取当前字段的值
+//     //替换空格，因为字符串拼接的时候如果遇到空格，会自动将后面的部分截掉，所有这里用html的转义符
+//     //&nbsp;代替
+//     values = values.replace(/\s+/g,'&nbsp;')
+//     return "<span title="+values+">"+row.email+"</span>"
+// }
